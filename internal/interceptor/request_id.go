50224b883403a561fc6a5b33d5ad70ff5668e4fc
Ellis James <contact@eljamo.com>
2024-05-21 14:36:53 +0100

    feature/use-context-and-update-interceptor (#2)
    * feat: update request id interceptor so it can return an error if request id is empty. This can be controlled by env var
    * feat: update GeneratePasswords() request to use context

--- internal/interceptor/request_id.go
+++ internal/interceptor/request_id.go
@@ -12,87 +12,96 @@ import (
 
 const HeaderXRequestID = "x-request-id"
 
+var (
+	ErrorRequestIDMissing     = errors.New("x-request-id header is missing")
+	ErrorFailedToSetRequestID = errors.New("failed to set x-request-id header")
+	ErrorInvalidRequestID     = errors.New("invalid x-request-id provided")
+)
+
 type errorStreamingClientInterceptor struct {
 	connect.StreamingClientConn
 	err error
 }
 
 type Interceptor struct {
-	logger *slog.Logger
+	allowEmptyRequestID bool
+	logger              *slog.Logger
 }
 
 var _ connect.Interceptor = &Interceptor{}
 
-func NewRequestIDInterceptor(logger *slog.Logger) *Interceptor {
+// NewRequestIDInterceptor creates a new Interceptor instance.
+func NewRequestIDInterceptor(allowEmptyRequestID bool, logger *slog.Logger) *Interceptor {
 	return &Interceptor{
-		logger: logger,
+		allowEmptyRequestID: allowEmptyRequestID,
+		logger:              logger,
 	}
 }
 
+// WrapUnary wraps a unary function with request ID validation.
 func (i *Interceptor) WrapUnary(next connect.UnaryFunc) connect.UnaryFunc {
 	return func(ctx context.Context, request connect.AnyRequest) (connect.AnyResponse, error) {
-		err := i.validateHeader(request.Header())
-		if err != nil {
+		if err := i.validateHeader(request.Header()); err != nil {
 			return nil, err
 		}
-
 		return next(ctx, request)
 	}
 }
 
+// WrapStreamingClient wraps a streaming client function with request ID validation.
 func (i *Interceptor) WrapStreamingClient(next connect.StreamingClientFunc) connect.StreamingClientFunc {
 	return func(ctx context.Context, spec connect.Spec) connect.StreamingClientConn {
 		conn := next(ctx, spec)
-		err := i.validateHeader(conn.RequestHeader())
-		if err != nil {
+		if err := i.validateHeader(conn.RequestHeader()); err != nil {
 			return &errorStreamingClientInterceptor{
 				StreamingClientConn: conn,
 				err:                 connect.NewError(connect.CodeInternal, err),
 			}
 		}
-
 		return conn
 	}
 }
 
+// WrapStreamingHandler wraps a streaming handler function with request ID validation.
 func (i *Interceptor) WrapStreamingHandler(next connect.StreamingHandlerFunc) connect.StreamingHandlerFunc {
 	return func(ctx context.Context, conn connect.StreamingHandlerConn) error {
-		err := i.validateHeader(conn.RequestHeader())
-		if err != nil {
+		if err := i.validateHeader(conn.RequestHeader()); err != nil {
 			return err
 		}
-
 		return next(ctx, conn)
 	}
 }
 
+// handleEmptyHeader handles cases where the x-request-id header is missing.
+func (i *Interceptor) handleEmptyHeader(headers http.Header) error {
+	if i.allowEmptyRequestID {
+		ulid, err := ulid.Generate()
+		if err != nil {
+			i.logger.Error("failed to set x-request-id header", slog.String("error", ErrorFailedToSetRequestID.Error()))
+			return connect.NewError(connect.CodeInternal, ErrorFailedToSetRequestID)
+		}
+		headers.Set(HeaderXRequestID, ulid)
+		i.logger.Info("set x-request-id header successfully", slog.String("id", ulid))
+	} else {
+		i.logger.Error("x-request-id header is missing")
+		return connect.NewError(connect.CodeInternal, ErrorRequestIDMissing)
+	}
+
+	return nil
+}
+
+// validateHeader validates and sets the x-request-id header.
 func (i *Interceptor) validateHeader(headers http.Header) error {
 	reqId := headers.Get(HeaderXRequestID)
 
 	if reqId == "" {
-		ulid, err := ulid.Generate()
-		if err != nil {
-			i.logger.Error("failed to set x-request-id", "err", err)
-
-			return connect.NewError(
-				connect.CodeInternal,
-				errors.New("failed to set x-request-id"),
-			)
-		}
-		headers.Set(HeaderXRequestID, ulid)
-
-		i.logger.Info("set x-request-id header successfully", "id", ulid)
+		return i.handleEmptyHeader(headers)
 	} else {
-		err := ulid.ValidateUlid(reqId)
-		if err != nil {
-			i.logger.Error("invalid x-request-id prodivded", "id", reqId)
-			return connect.NewError(
-				connect.CodeInternal,
-				errors.New("invalid x-request-id provided"),
-			)
+		if err := ulid.ValidateUlid(reqId); err != nil {
+			i.logger.Error("invalid x-request-id provided", slog.String("id", reqId))
+			return connect.NewError(connect.CodeInternal, ErrorInvalidRequestID)
 		}
-
-		i.logger.Info("validated x-request-id header successfully", "id", reqId)
+		i.logger.Info("validated x-request-id header successfully", slog.String("id", reqId))
 	}
 
 	return nil
