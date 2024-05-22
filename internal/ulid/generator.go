09d7fb5ea64a00fa11ad3eef53917c2204bbb2b8
Ellis James <contact@eljamo.com>
2024-05-22 16:18:33 +0100

    feature/add-custom-golangci-config (#3)
    * ci: add .golangci.toml
    * refactor: update code based off golangci-lint output using the a custom config
    * build(deps): update libpass to v7.1.2
    * build(deps): update indirect deps

--- internal/ulid/generator.go
+++ internal/ulid/generator.go
@@ -2,6 +2,7 @@ package ulid
 
 import (
 	"crypto/rand"
+	"fmt"
 
 	"github.com/oklog/ulid/v2"
 )
@@ -9,7 +10,7 @@ import (
 func Generate() (string, error) {
 	ulid, err := ulid.New(ulid.Now(), rand.Reader)
 	if err != nil {
-		return "", err
+		return "", fmt.Errorf("failed to generate ULID: %w", err)
 	}
 
 	return ulid.String(), nil
@@ -18,7 +19,7 @@ func Generate() (string, error) {
 func ValidateUlid(s string) error {
 	_, err := ulid.Parse(s)
 	if err != nil {
-		return err
+		return fmt.Errorf("invalid ULID: %w", err)
 	}
 
 	return nil
